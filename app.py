51c96ba57e2e061895cdfe6a9dc1fec30a702a76
Alison Thomson <Alison.Thomson@softwire.com>
2025-01-22 15:15:54 +0000

    Module 13: Remove live metrics

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from azure.monitor.opentelemetry import configure_azure_monitor
 import logging
 
 logging.basicConfig(level=logging.INFO)
-configure_azure_monitor(enable_live_metrics = True)
+configure_azure_monitor()
 
 from flask import Flask, render_template, request
 from datetime import datetime, timezone
